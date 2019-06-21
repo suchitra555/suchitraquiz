6f3f314661d8da0af0816fd47d511e86667b6864
suchitra <suchitra.nagarajan@mavs.uta.edu>
2019-06-20 19:41:20 -0600

    suchi

--- application.py
+++ application.py
@@ -51,107 +51,120 @@ def list():
    cur = con.cursor()
    dep1 = float(request.form['dep1'])
    dep2 = float(request.form['dep2'])
-   lon = float(request.form['lon'])
-   cur.execute("select latitude,longitude,time,depthError from quake6 where longitude > ? and depthError between ? and ?",(lon,dep1,dep2))
+   cur.execute("select StateName from voting_modified where TotalPop  between 2000 and 8000")
    rows = cur.fetchall()
    con.close()
    return render_template("list.html",rows = rows)
 
-@app.route('/options4', methods = ['POST', 'GET'])
-def options4():
+@app.route('/list1', methods = ['POST', 'GET'])
+def list1():
    con = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
    cur = con.cursor()
-   num = int(request.form['num'])
-   for i in range(num):
-       val = round(random.uniform(10.0,12.5),1)
-       val2 = round(random.uniform(10.0,12.5),1)
-       cur.execute("select latitude,longitude,time,depthError from quake6 where depthError between ? and ?",(val,val2))
-       rows = cur.fetchall()
-       # print(rows)
+   dep1 = float(request.form['dep1'])
+   dep2 = float(request.form['dep2'])
+   cur.execute("select StateName from voting_modified where TotalPop  between 8000 and 40000")
+   rows = cur.fetchall()
    con.close()
-   st = []
-   st.append(rows)
-   return render_template("lists1.html",rows = st)
+   return render_template("list3.html",rows = rows)
 
-@app.route('/options5', methods=['POST', 'GET'])
-def options5():
-    cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-    r = redis.StrictRedis(host=myHostname, port=6380, password=myPassword, ssl=True)
-    start_time = time.time()
-    num = int(request.form['num'])
-    depth1 = float(request.form['depth1'])
-    depth2 = float(request.form['depth2'])
-    opt = request.form['opt']
-
-    rows = []
-    c = []
-    if (opt == 'n' or 'all'):
-        print("-- n ---")
-        for i in range(num):
-
-            val1 = random.uniform(depth1, depth2)
-            val2 = random.uniform(val1, depth2)
-            cur = cnxn.cursor()
-            a = "select latitude, longitude, time, depth from quake where depth between " + str(val1) + " and " + str(val2)
-
-            cur.execute("select latitude, longitude, time, depth from quake where depth between ? and  ?",
-                    (str(val1), str(val2),))
-            get = cur.fetchall()
-            rows.append(get)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-
-    if (opt == 'im' or 'all'):
-        for i in range(num):
-            val1 = random.uniform(depth1, depth2)
-            val2 = random.uniform(val1, depth2)
-            cur = cnxn.cursor()
-            a = "select latitude, longitude, time, depth from quake where depth between " + str(val1) + " and " + str(
-                val2)
-            if r.get(a):
-                c.append('Cached')
-                rows.append(r.get(a))
-            else:
-                c.append('Not Cached')
-                cur.execute("select latitude, longitude, time, depth from quake where depth between ? and  ?",
-                            (str(val1), str(val2),))
-                get = cur.fetchall()
-                rows.append(get)
-                r.set(a, str(get))
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-
-    return render_template("list1.html", rows=rows, elapsed_time=elapsed_time)
-
-@app.route('/options6', methods=['POST', 'GET'])
-def options6():
-    cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-    r = redis.StrictRedis(host=myHostname, port=6380, password=myPassword, ssl=True)
-    start_time = time.time()
-    num = int(request.form['num'])
-    depth1 = float(request.form['depth1'])
-    depth2 = float(request.form['depth2'])
-
-    rows = []
-    c = []
-    for i in range(num):
-        val1 = random.uniform(depth1, depth2)
-        val2 = random.uniform(val1, depth2)
-        cur = cnxn.cursor()
-        a = "select latitude, longitude, time, depth from quake where depth between "+str(val1)+" and "+str(val2)
-        if r.get(a):
-            c.append('Cached')
-            rows.append(r.get(a))
-        else:
-            c.append('Not Cached')
-            cur.execute("select latitude, longitude, time, depth from quake where depth between ? and  ?",
-                (str(val1), str(val2),))
-            get = cur.fetchall()
-            rows.append(get)
-            r.set(a, str(get))
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    return render_template("list1.html", rows=rows, elapsed_time=elapsed_time)
+# @app.route('/options4', methods = ['POST', 'GET'])
+# def options4():
+#    con = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+#    cur = con.cursor()
+#    start_time = time.time()
+#    num = int(request.form['num'])
+#    for i in range(num):
+#        val = round(random.uniform(10.0,12.5),1)
+#        val2 = round(random.uniform(10.0,12.5),1)
+#        cur.execute("select latitude,longitude,time,depthError from quake6 where depthError between ? and ?",(val,val2))
+#        rows = cur.fetchall()
+#        # print(rows)
+#    con.close()
+#    st = []
+#    st.append(rows)
+#    end_time = time.time()
+#    elapsed_time = end_time - start_time
+#    return render_template("lists1.html",rows = st,elapsedtime=elapsed_time)
+#
+# @app.route('/options5', methods=['POST', 'GET'])
+# def options5():
+#     cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+#     r = redis.StrictRedis(host=myHostname, port=6380, password=myPassword, ssl=True)
+#     start_time = time.time()
+#     num = int(request.form['num'])
+#     depth1 = float(request.form['depth1'])
+#     depth2 = float(request.form['depth2'])
+#     opt = request.form['opt']
+#
+#     rows = []
+#     c = []
+#     if (opt == 'n' or 'all'):
+#         print("-- n ---")
+#         for i in range(num):
+#
+#             val1 = random.uniform(depth1, depth2)
+#             val2 = random.uniform(val1, depth2)
+#             cur = cnxn.cursor()
+#             a = "select latitude, longitude, time, depth from quake where depth between " + str(val1) + " and " + str(val2)
+#
+#             cur.execute("select latitude, longitude, time, depth from quake where depth between ? and  ?",
+#                     (str(val1), str(val2),))
+#             get = cur.fetchall()
+#             rows.append(get)
+#         end_time = time.time()
+#         elapsed_time = end_time - start_time
+#
+#     if (opt == 'im' or 'all'):
+#         for i in range(num):
+#             val1 = random.uniform(depth1, depth2)
+#             val2 = random.uniform(val1, depth2)
+#             cur = cnxn.cursor()
+#             a = "select latitude, longitude, time, depth from quake where depth between " + str(val1) + " and " + str(
+#                 val2)
+#             if r.get(a):
+#                 c.append('Cached')
+#                 rows.append(r.get(a))
+#             else:
+#                 c.append('Not Cached')
+#                 cur.execute("select latitude, longitude, time, depth from quake where depth between ? and  ?",
+#                             (str(val1), str(val2),))
+#                 get = cur.fetchall()
+#                 rows.append(get)
+#                 r.set(a, str(get))
+#         end_time = time.time()
+#         elapsed_time = end_time - start_time
+#
+#     return render_template("list1.html", rows=rows, elapsed_time=elapsed_time)
+#
+# @app.route('/options6', methods=['POST', 'GET'])
+# def options6():
+#     cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+#     r = redis.StrictRedis(host=myHostname, port=6380, password=myPassword, ssl=True)
+#     start_time = time.time()
+#     num = int(request.form['num'])
+#     depth1 = float(request.form['depth1'])
+#     depth2 = float(request.form['depth2'])
+#
+#     rows = []
+#     c = []
+#     for i in range(num):
+#         val1 = random.uniform(depth1, depth2)
+#         val2 = random.uniform(val1, depth2)
+#         cur = cnxn.cursor()
+#         a = "select latitude, longitude, time, depth from quake where depth between "+str(val1)+" and "+str(val2)
+#         if r.get(a):
+#             c.append('Cached')
+#             rows.append(r.get(a))
+#         else:
+#             c.append('Not Cached')
+#             cur.execute("select latitude, longitude, time, depth from quake where depth between ? and  ?",
+#                 (str(val1), str(val2),))
+#             get = cur.fetchall()
+#             rows.append(get)
+#             r.set(a, str(get))
+#     end_time = time.time()
+#     elapsed_time = end_time - start_time
+#     return render_template("list1.html", rows=rows, elapsed_time=elapsed_time)
 
 
 if __name__ == '__main__':
